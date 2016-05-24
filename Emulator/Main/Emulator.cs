2759bc6818549a163a387ce2173abd5b8e1b3192
Mateusz Holenko <mholenko@antmicro.com>
2016-05-24 15:13:12 +0200

    Unify the way Emul8 shuts down.

--- Emulator/Main/Emulator.cs
+++ Emulator/Main/Emulator.cs
@@ -5,16 +5,43 @@
 // This file is part of the Emul8 project.
 // Full license details are defined in the 'LICENSE' file.
 //
+using System;
+using System.Threading;
+using Emul8.Core;
 using Emul8.Exceptions;
+using Emul8.Logging;
 using Emul8.UserInterface;
 using System.Collections.Concurrent;
 using System;
 using System.Threading;
+using Emul8.Utilities;
 
 namespace Emul8
 {
     public static class Emulator
     {
+        public static void Exit()
+        {
+            var beforeExit = BeforeExit;
+            if(beforeExit != null)
+            {
+                beforeExit();
+            }
+            exitEvent.Set();
+        }
+
+        public static void WaitForExit()
+        {
+            exitEvent.Wait();
+        }
+
+        public static void DisposeAll()
+        {
+            EmulationManager.Instance.Clear();
+            TypeManager.Instance.Dispose();
+            Logger.Dispose();
+        }
+
         public static IUserInterfaceProvider UserInterfaceProvider
         {
             get
@@ -50,8 +77,11 @@ namespace Emul8
             actionsOnMainThread.CompleteAdding();
         }
 
-        private static readonly BlockingCollection<Action> actionsOnMainThread = new BlockingCollection<Action>();
+        public static event Action BeforeExit;
+
         private static IUserInterfaceProvider userInterfaceProvider;
+        private static ManualResetEventSlim exitEvent = new ManualResetEventSlim();
+        private static readonly BlockingCollection<Action> actionsOnMainThread = new BlockingCollection<Action>();
     }
 }
 
